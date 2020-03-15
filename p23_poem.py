979e4248054a4f6201dd570112085f8f1f247a7f
zhangcj5131 <zhangcj5@gmail.com>
2020-03-15 11:12:21 +0800

    poem generator_v1

--- p23_poem.py
+++ p23_poem.py
@@ -18,7 +18,7 @@ class Config:
         self.logdir = 'logs/{name}/'.format(name=self.name)
 
         self.lr = 0.0002
-        self.epoches = 100
+        self.epoches = 200
 
     def get_gpus(self):
         value = os.getenv('CUDA_VISIBLE_DEVICES', '0')
@@ -48,97 +48,99 @@ class Tensors:
 
         with tf.variable_scope('poem'):
             for gpu_index in range(config.gpus):
-                self.sub_tensors.append(SubTensors(config, gpu_index, opt, char_size))
+                self.sub_tensors.append(Sub_tensors(config, gpu_index, opt, char_size))
                 tf.get_variable_scope().reuse_variables()
 
         with tf.device('/gpu:0'):
             grad = self.merge_grads()
             self.train_op = opt.apply_gradients(grad)
-
             self.loss = tf.reduce_mean([ts.loss for ts in self.sub_tensors])
             self.precise = tf.reduce_mean([ts.precise for ts in self.sub_tensors])
-
             tf.summary.scalar('loss', self.loss)
             tf.summary.scalar('precise', self.precise)
-
             self.summary_op = tf.summary.merge_all()
 
-
     def merge_grads(self):
-        index_grads = {}
         grads = {}
+        indexed_grads = {}
         for ts in self.sub_tensors:
             for g, v in ts.grad:
                 if isinstance(g, tf.IndexedSlices):
-                    if v not in index_grads:
-                        index_grads[v] = []
-                    index_grads[v].append(g)
+                    if v not in indexed_grads:
+                        indexed_grads[v] = []
+                    indexed_grads[v].append(g)
                 else:
                     if v not in grads:
                         grads[v] = []
                     grads[v].append(g)
         result = [(tf.reduce_mean(grads[v], axis = 0), v) for v in grads]
-
-        for v in index_grads:
-            indices = tf.concat([g.indices for g in index_grads[v]], axis = 0)
-            values = tf.concat([g.values for g in index_grads[v]], axis = 0)
+        for v in indexed_grads:
+            indices = tf.concat([g.indices for g in indexed_grads[v]], axis = 0)
+            values = tf.concat([g.values for g in indexed_grads[v]], axis = 0)
             g = tf.IndexedSlices(values, indices)
             result.append((g, v))
+
         return result
 
-class SubTensors:
+class Sub_tensors:
     def __init__(self, config: Config, gpu_index, opt: tf.train.AdamOptimizer, char_size):
         with tf.device('/gpu:%d' % gpu_index):
-            self.x = tf.placeholder(tf.int32, [None, config.num_step], 'x')#-1, 32
+            self.x = tf.placeholder(tf.int32, [None, config.num_step], 'x') #-1, 32
             char_dict = tf.get_variable('char_dict', [char_size, config.num_units], tf.float32)#4000, 200
-            x = tf.nn.embedding_lookup(char_dict, self.x) #-1, 32, 200
+            x = tf.nn.embedding_lookup(char_dict, self.x)#-1, 32, 200
 
-            cell1 = tf.nn.rnn_cell.BasicLSTMCell(config.num_units, name = 'lstm1')
-            cell2 = tf.nn.rnn_cell.BasicLSTMCell(config.num_units, name='lstm2')
+            cell1 = tf.nn.rnn_cell.BasicLSTMCell(config.num_units, state_is_tuple=True, name = 'lstm1')
+            cell2 = tf.nn.rnn_cell.BasicLSTMCell(config.num_units, state_is_tuple=True, name='lstm2')
 
             cell = tf.nn.rnn_cell.MultiRNNCell([cell1, cell2])
 
             batch_size_tf = tf.shape(self.x)[0]
             state = cell.zero_state(batch_size_tf, tf.float32)
 
-            y = tf.concat([self.x[:, 1:], tf.zeros([batch_size_tf, 1], tf.int32)], axis = 1)
-            y_one_hot = tf.one_hot(y, char_size)
+            y = tf.concat([self.x[:, 1:], tf.zeros([batch_size_tf, 1], tf.int32)], axis = 1)#-1, 32
+            y_onehot = tf.one_hot(y, char_size)#-1, 32, 4000
 
             loss_list = []
             precise_list = []
             with tf.variable_scope('rnn'):
                 for i in range(config.num_step):
-                    xi = x[:, i]
-                    y_pre, state = cell(xi, state)
+                    xi = x[:, i]#-1, 200
+                    yi_pred, state = cell(xi, state)
+                    yi_pred = tf.layers.dense(yi_pred, char_size, name = 'yi_predict')#-1, char_size
+                    yi_onehot = y_onehot[:, i]#-1, 4000
 
-                    y_pre = tf.layers.dense(y_pre, char_size, name = 'y_dense')
-
-                    yi = y_one_hot[:, i]
-
-                    loss = tf.nn.softmax_cross_entropy_with_logits_v2(yi, y_pre)
+                    loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=yi_pred, labels=yi_onehot)
                     loss_list.append(loss)
 
-                    y_predict = tf.math.argmax(y_pre, axis = 1, output_type=tf.int32)
-                    y_label = y[:, i]
-                    precise = tf.equal(y_predict, y_label)
+                    yi_predict_id = tf.math.argmax(yi_pred, axis = 1, output_type=tf.int32)
+                    yi_label_id = y[:, i]
+                    precise = tf.equal(yi_label_id, yi_predict_id)
                     precise = tf.cast(precise, tf.float32)
                     precise = tf.reduce_mean(precise)
                     precise_list.append(precise)
                     tf.get_variable_scope().reuse_variables()
+            self.loss = tf.reduce_mean(loss_list)
+            self.precise = tf.reduce_mean(precise_list)
+            self.grad = opt.compute_gradients(self.loss)
 
 
-        self.loss = tf.reduce_mean(loss_list)
-        self.precise = tf.reduce_mean(precise_list)
-        self.grad = opt.compute_gradients(self.loss)
+            self.batch_size = tf.placeholder(tf.int32, [], 'batch_size')
+            self.xi = tf.placeholder(tf.int32, [None], 'xi')
+            self.inputstate = cell.zero_state(self.batch_size, tf.float32)
+            with tf.variable_scope('rnn', reuse=True):
+                xi = tf.nn.embedding_lookup(char_dict, self.xi)
+                y_predict, self.state = cell(xi, self.inputstate)
+                y_predict = tf.layers.dense(y_predict, char_size, name = 'yi_predict')
+                self.y_predict = tf.math.argmax(y_predict, axis = 1)
 
-        self.batch_size = tf.placeholder(tf.int32, [], 'batch_size')
-        self.xi = tf.placeholder(tf.int32, [None], 'xi')
-        self.input_state = cell.zero_state(self.batch_size, tf.float32)
-        with tf.variable_scope('rnn', reuse=True):
-            xi = tf.nn.embedding_lookup(char_dict, self.xi)
-            y_pred, self.state = cell(xi, self.input_state)
-            y_pred = tf.layers.dense(y_pred, char_size, name = 'y_dense')
-            self.y_predict = tf.math.argmax(y_pred, axis = 1)
+
+
+
+
+
+
+
+
 
 
 
@@ -148,65 +150,60 @@ class Chinese:
         f = open(config.sample_path, encoding='utf-8')
         with f:
             poems = f.readlines()
-        result = []
         char_set = set()
+        result = []
         for poem in poems:
             poem = poem.rstrip()
             result.append(poem)
             for ch in poem:
                 char_set.add(ch)
-
         self.char_id_dict = {}
         self.id_char_dict = {}
-        for id, ch in enumerate(char_set):
-            self.char_id_dict[ch] = id
-            self.id_char_dict[id] = ch
+        for id, char in enumerate(sorted(char_set)):
+            self.char_id_dict[char] = id
+            self.id_char_dict[id] = char
+
         self.poems = [[self.char_id_dict[ch] for ch in poem] for poem in result]
 
-    @property
-    def char_size(self):
-        return len(self.char_id_dict)
 
     def get_random_char(self):
         id = np.random.randint(0, self.char_size)
         return self.id_char_dict[id]
 
-    def encode(self, head):
-        return [self.char_id_dict[ch] for ch in head]
+    def encode(self, char_list):
+        return [self.char_id_dict[ch] for ch in char_list]
 
-    def decode(self, char_list):
-        # print(char_list)
+    def decode(self, id_list):
         result = ''
-        # print(self.id_char_dict)
-        # print(self.char_id_dict)
-        # print(self.id_char_dict[1630])
-        # print(self.id_char_dict[5846])
-        for id in char_list:
-            # print(id, self.id_char_dict[id])
+        for id in id_list:
             result += self.id_char_dict[id]
         return result
 
 
+    @property
+    def char_size(self):
+        return len(self.char_id_dict)
+
 class Samples(Chinese):
     def __init__(self, config: Config):
         super(Samples, self).__init__(config)
-        self.config = config
         self.index = 0
-
-    @property
-    def size(self):
-        return len(self.poems)
 
     def next_batch(self, batch_size):
         next = self.index + batch_size
-        if next < self.num:
-            x = self.poems[self.index: next]
+        if next < self.poem_size:
+            result = self.poems[self.index: next]
         else:
-            x = self.poems[self.index:]
-            next -= self.num
-            x += self.poems[:next]
+            result = self.poems[self.index:]
+            next -= self.poem_size
+            result += self.poems[:next]
         self.index = next
-        return x
+        return result
+
+    @property
+    def poem_size(self):
+        return len(self.poems)
+
 
 
 class Poem:
@@ -214,58 +211,59 @@ class Poem:
         self.config = config
         graph = tf.Graph()
         with graph.as_default():
+            conf = tf.ConfigProto()
+            conf.allow_soft_placement = True
+            self.session = tf.Session(config=conf, graph=graph)
             self.samples = Samples(config)
             self.tensors = Tensors(config, self.samples.char_size)
-            cfg = tf.ConfigProto()
-            cfg.allow_soft_placement = True
-            self.session = tf.Session(config=cfg, graph=graph)
+            self.file_write = tf.summary.FileWriter(logdir=config.logdir, graph=graph)
             self.saver = tf.train.Saver()
-            self.file_writer = tf.summary.FileWriter(config.logdir, graph=graph)
             try:
                 self.saver.restore(self.session, config.save_path)
-                print('Restore the model from %s successfully.' % config.save_path)
             except:
-                print('Fail to restore the model from', config.save_path)
                 self.session.run(tf.global_variables_initializer())
 
     def train(self):
-        cfg = self.config
-        batches = self.samples.num // (cfg.gpus * cfg.batch_size)
-        for epoch in range(cfg.epoches):
+        config = self.config
+        batches = self.samples.poem_size // (config.gpus * config.batch_size)
+        for epoch in range(config.epoches):
             for batch in range(batches):
                 feed_dict = {
-                    self.tensors.lr: cfg.lr
+                    self.tensors.lr:config.lr
                 }
-                for gpu_index in range(cfg.gpus):
-                    x = self.samples.next_batch(cfg.batch_size)
+                for gpu_index in range(config.gpus):
+                    x = self.samples.next_batch(config.batch_size)
                     feed_dict[self.tensors.sub_tensors[gpu_index].x] = x
-                    _, precise, lo, su = self.session.run([self.tensors.train_op,
-                                      self.tensors.precise,
+                    _, loss, precise, su = self.session.run([self.tensors.train_op,
                                       self.tensors.loss,
+                                      self.tensors.precise,
                                       self.tensors.summary_op], feed_dict)
-                    self.file_writer.add_summary(su, epoch*batches + batch)
-                    print('%d/%d, loss = %f, precise = %f' % (epoch, batch, lo, precise))
-            self.saver.save(self.session, cfg.save_path)
+                    self.file_write.add_summary(su, epoch*batches + batch)
+                    print('%d/%d, loss = %f, precise = %f' % (epoch, batch, loss, precise))
+            self.saver.save(self.session, config.save_path)
 
-    def predict(self, head):
+    def close(self):
+        self.session.close()
+
+    def predict(self, head=None):
         if head is None:
             head = self.samples.get_random_char()
         head = self.samples.encode(head)
-        
         result = head
-        state = self.session.run([self.tensors.sub_tensors[0].input_state], {self.tensors.sub_tensors[0].batch_size:1})
+
+        state = self.session.run(self.tensors.sub_tensors[0].inputstate, {self.tensors.sub_tensors[0].batch_size:1})
         y_predict = None
-        for i in range(config.num_step):
+        for i in range(self.config.num_step):
             xi = [head[i]] if i < len(head) else y_predict
-            if i >= len(head):
-                result.append(y_predict[0])
+
             feed_dict = {
                 self.tensors.sub_tensors[0].xi: xi,
-                self.tensors.sub_tensors[0].input_state: state
+                self.tensors.sub_tensors[0].inputstate: state
             }
+            if i >= len(head):
+                result.append(y_predict[0])
             y_predict, state = self.session.run([self.tensors.sub_tensors[0].y_predict,
                                                  self.tensors.sub_tensors[0].state], feed_dict)
-        # print(result)
         result = self.samples.decode(result)
         return result
 
@@ -277,8 +275,11 @@ class Poem:
 
 if __name__ == '__main__':
     config = Config()
-    # s = Samples(config)
-    # t = Tensors(config, s.size)
+    s = Samples(config)
+    # Tensors(config, s.char_size)
     poem = Poem(config)
-    result = poem.predict('我')
-    print(result)
+    # poem.train()
+    print(poem.predict())
+    poem.close()
+
+
